0f3cbf883399b35d1e24ad2776fb8b242b80728b
VelkyCajicek <peichl294@gmail.com>
2025-01-19 11:57:44 +0100

    Updated README and added point density heatmap

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Internship-2025
- 
+
+## Literature
+
+Jiří Matoušek - Geometric Discrepancy : https://people.fjfi.cvut.cz/vybirja2/TUM-IBC/Matousek_Geometric_Discrepancy.pdf
+
+## Formulas for D*
+
+### ChatGPT D* for 2d (Complicated)
+This is the general formula he gave me:
+$$D^*(P) = \begin{matrix}sup \\ (x,y) \in[0,1)^2 \end{matrix}  \left\{ \sum_{(x_i, y_i) \in P} 1_{[0,x) \times [0,y)}(x_i, y_i) - xy \mid (x, y) \in [0, 1)^2  \right\}$$
+
+### Meeting notes (Simpler)
+
+This is the more complicated version:
+
+$$D_{N}^{*} = \begin{matrix}sup \\ B\in  \mathbf{B} \end{matrix}\left | \frac{(P\cap B)}{N} - V(B)\right |$$
+
+This is the simplified version:
+
+$$D_{N}^{*} = \begin{matrix}max \\ B\in  \mathbf{B} \end{matrix}\left | \frac{n}{N} - \frac{a}{A}\right | $$
+
+This is what the following symbols/characters mean : 
+- sup and max -> Same thing  (supremum = nejmenší hodnota, která je větší než všechny ostatní :P)  
+- B and **B** -> Represents all the possible shapes the area of the region can have (doesnt really matter)
+- n and N -> N is the number of all points and n is the number of points inside of area we are checking
+- a and A -> A is the total area and a is the area of the part we are checking
--- Transformations/2D_plotting.py
+++ Transformations/2D_plotting.py
@@ -2,7 +2,6 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import re
-
 # Changes path of working directory
 path = os.path.realpath(__file__)
 dir = os.path.dirname(path)
@@ -77,31 +76,74 @@ def generate_points(points_x : list, points_y : list, axis_string : str):
     for i in range(len(coordinates)):
         temp_coords = coordinates[i].split(",")
         for value in range(0, 100):
-            points_x.append(coordinate_shift(min_x, min_y, max_x, max_y, eval(temp_coords[0].replace("x", str(value / 100)))))
-            points_y.append(coordinate_shift(min_x, min_y, max_x, max_y, None, eval(temp_coords[1].replace("x", str(value / 100)))))
+            # Rounding may be a mistake
+            x_point = round(eval(temp_coords[0].replace("x", str(value / 100))), 2)
+            y_point = round(eval(temp_coords[1].replace("x", str(value / 100))), 2)
+            
+            #points_x.append(coordinate_shift(min_x, min_y, max_x, max_y, x_point))
+            #points_y.append(coordinate_shift(min_x, min_y, max_x, max_y, None, y_point))            
+            
+            # Assuming there cant be more than 1 point in one spot
+            if(check_for_point(points_x, points_y, x_point, y_point)): 
+                # Assuming multiple points go in the same spot
+                points_x.append(coordinate_shift(min_x, min_y, max_x, max_y, x_point))
+                points_y.append(coordinate_shift(min_x, min_y, max_x, max_y, None, y_point))
+            else:
+                continue
+
+def check_for_point(points_x : list, points_y : list, x_point : float, y_point : float):
+    for i in range(len(points_x)):
+        if(points_x[i] == x_point):
+            if(points_y[i] == y_point):
+                return False
+    return True
 
 def coordinate_shift(min_x, min_y, max_x, max_y, x = None, y = None):
-    if(x != None): return float((x - min_x) / (max_x - min_x))
-    else: return float((y - min_y) / (max_y - min_y))
+    # Try must be here if there is no coordinate shift and you have the point [0,0]
+    # This is all assuming points with a different letter arent moved
+    try:
+        if(x != None): return float((x - min_x) / (max_x - min_x))
+        else: return float((y - min_y) / (max_y - min_y))
+    except(ZeroDivisionError):
+        return float(0)
+
+def local_star_discrepancy():
+    pass
 
 if __name__ == "__main__":
     points_x = []
     points_y = []
     multiplicities = []
+    heatmap_bool = True
+    # User input here for now
+    test_input = "11fea"
     
-    test_input = "11efa"
     user_input_list = obtain_user_input(test_input)
     axis_strings, multiplicities = obtain_axis_data(user_input_list)
+    # Must do this since fstrings hate "".join() for some reason
     concatenated_user_input_list = " ".join([str(value) for value in user_input_list])
     
     for i in range(len(axis_strings)):
         generate_points(points_x, points_y, axis_strings[i])
-    
+    # Convert lists to numpy arrays for graphing
     x_axis = np.array(points_x)
     y_axis = np.array(points_y)
 
-    plt.scatter(x_axis,y_axis)
-    plt.title(f"{concatenated_user_input_list} (N = {sum(multiplicities)}, A = 2)")
-    plt.xlabel(f"{user_input_list[0]}{user_input_list[1]}")
-    plt.ylabel(f"{user_input_list[0]}{user_input_list[2]}")
+    if(heatmap_bool):
+        # Heatmap
+        heatmap, xedges, yedges = np.histogram2d(x_axis, y_axis, bins=50)
+        extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+        plt.clf()
+        # Good cmaps: seismic, PiYG 
+        plt.imshow(heatmap.T, extent=extent, origin='lower', interpolation='bilinear', cmap="seismic")
+        plt.title(f"{concatenated_user_input_list} (N = {sum(multiplicities)}, A = 2)")
+        plt.xlabel(f"{user_input_list[0]}{user_input_list[1]}")
+        plt.ylabel(f"{user_input_list[0]}{user_input_list[2]}")
+    else:
+        # Scatter plot
+        plt.scatter(x_axis,y_axis)
+        plt.title(f"{concatenated_user_input_list} (N = {sum(multiplicities)}, A = 2)")
+        plt.xlabel(f"{user_input_list[0]}{user_input_list[1]}")
+        plt.ylabel(f"{user_input_list[0]}{user_input_list[2]}")
+
     plt.show()    
