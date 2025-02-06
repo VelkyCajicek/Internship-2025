fe5f5583002240f74577ee6babbe01a652f48e03
VelkyCajicek <144894564+VelkyCajicek@users.noreply.github.com>
2025-02-06 09:40:31 +0100

    Update Bundschuh_Zhu_Algorithm.py

--- Star_Discrepancy/Bundschuh_Zhu_Algorithm.py
+++ Star_Discrepancy/Bundschuh_Zhu_Algorithm.py
@@ -1,43 +1,79 @@
 import itertools
+import time
 
 def create_test_case(format_value : int):
     points_x = [k/32 for k in range(0,32)]
-    points_y = [round((7*k/32) % 1, 8) for k in range(0,32)]
-    # Formats of test case
-    # 1. Simply returns points_x and points_y 
-    # 2. Returns a combined list ([x1, y1, ... ,xn, yn])
-    # 3. Returns a list with coordinate formated values ([[x1,y1], ... , [xn, yn]])
+    points_y = [round((7*k/32) % 1, 4)  for k in range(0,32)]
+    # Point Shift
     match(format_value):
+        # 1. Two seperate lists 
         case 1: return points_x, points_y
+        # 2. One list with alternating values
         case 2: return list(itertools.chain.from_iterable(zip(points_x,points_y)))
-        #case 3: return [
+        # 3. One list with coordinate style values (currently with lists) 
+        case 3: return [list(coordinate) for coordinate in zip(points_x, points_y)]
 
-#def Bundschuh_Zhu_Algorithm_Wolfgang():
-    
+# Function takes pointset with format 3
+# Returns correct value
+def Bundschuh_Zhu_Algorithm_WH(pointset : list) -> float:
+    n = len(pointset)
+    # Sorts pointset based on x values
+    sorted_pointset = sorted(pointset, key=lambda coord : coord[0])
+    # Extracts x and y values
+    x_values = [0.0] + [entry[0] for entry in sorted_pointset] + [1.0]
+    y_values = [entry[1] for entry in sorted_pointset]
+    # Creates y value matrix
+    matrix_y = [[0.0] + sorted(y_values[0 : l]) + [1.0] for l in range(0, n+1)]
+    # Discrepancy calculation
+    max_discrepancy = 0.0
+    for l in range(0, n+1):
+        for k in range(0, l+1):
+            # Calculation from the formula
+            point_ratio = k / n
+            area_one = x_values[l] * matrix_y[l][k]
+            area_two = x_values[l+1] * matrix_y[l][k+1]
+            discrepancy = max(point_ratio - area_one, area_two - point_ratio)
+            # If discrepancy is larger append it
+            if(discrepancy > max_discrepancy):
+                max_discrepancy = discrepancy
+    return round(max_discrepancy, 8)
 
-def Bundschuh_Zhu_Algorithm_lists(points_x, points_y):
-    n = len(points_x)
-    points_y_sorted = [x for _,x in sorted(zip(points_x,points_y))]
-    points_x = [0.0] + points_x + [1.0]
-    
-    l_list = []
-    for l in range(0,n+1):
-        k_list = []
-        # For now testing this
-        try:
-            xi_matrix = sorted(list(points_y_sorted[0:l])) + [1.0] # [0.0] + 
-        except(TypeError):
-            xi_matrix = [0.0, 1.0]
-        # Algorithm itself
-        for k in range(0,l+1):
-            k_list.append(max(abs(k/n - points_x[l]*xi_matrix[k]), abs(k/n - points_x[l+1]*xi_matrix[k+1])))
-        l_list.append(max(k_list))
-    
-    return max(k_list)
+# My attempt at optimalizing this
+def Bundschuh_Zhu_Algorithm_TP(pointset : list) -> float:
+    n = len(pointset)
+    # Still unsure how necessary this is 
+    # Sorts pointset based on x values
+    sorted_pointset = sorted(pointset, key=lambda coord : coord[0])
+    # Extracts x and y values
+    x_values = [0.0] + [entry[0] for entry in sorted_pointset] + [1.0]
+    y_values = [entry[1] for entry in sorted_pointset]
+    max_discrepancy = 0
+    for l in range(0, n+1):
+        # Moved this calculation inside of calculation since it may be better for other programming languages
+        matrix_y = [0.0] + sorted(y_values[0 : l]) + [1.0]
+        for k in range(0, l+1):
+            # Calculation from the formula
+            point_ratio = k / n
+            area_one = x_values[l] * matrix_y[k]
+            area_two = x_values[l+1] * matrix_y[k+1]
+            discrepancy = max(point_ratio - area_one, area_two - point_ratio)
+            # If discrepancy is larger append it
+            if(discrepancy > max_discrepancy):
+                max_discrepancy = discrepancy
+    return round(max_discrepancy, 8)
+
+def Bundschuh_Zhu_Algorithm_3D(pointset : list) -> float:
+    pass
 
 if __name__ == "__main__":
-    points_x, points_y = create_test_case(1)
-    #print(Bundschuh_Zhu_Algorithm_lists(points_x, points_y))
-    result = [[x,y] for x,y in list(itertools.chain.from_iterable(zip(points_x,points_y)))]
-    print(result)
-    
+    # SUMMARY
+    # Times are virtually the same, tried timing them and it returned inconsistent values (differences were different)
+    # Biggest difference were these values:
+    # TP - --- 0.002983570098876953 seconds ---
+    # WH - --- 0.003998279571533203 seconds ---
+    start_time = time.time()
+    print(Bundschuh_Zhu_Algorithm_TP(create_test_case(3)))
+    print("--- %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+    print(Bundschuh_Zhu_Algorithm_WH(create_test_case(3)))
+    print("--- %s seconds ---" % (time.time() - start_time))
